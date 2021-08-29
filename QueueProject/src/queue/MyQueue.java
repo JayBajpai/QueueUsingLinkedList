b092726cba0b61a832a582f03bf8fa177d766eb2
Jay22031999 <Jay22031999>
2021-08-29 13:33:22 +0530

    queue created successfully

--- QueueProject/src/queue/MyQueue.java
+++ QueueProject/src/queue/MyQueue.java
@@ -2,8 +2,42 @@ package queue;
 
 import linkedlist.INode;
 import stack.MyLinkedList;
+
 public class MyQueue {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Queue Master Branch");
+	
+	private final MyLinkedList MyLinkedList;
+	private INode myNode;
+	public MyQueue(){
+		
+		this.MyLinkedList = new MyLinkedList();
 	}
-}
+	
+	
+	public void enqueue(INode myNode) {
+		
+		MyLinkedList.append(myNode);
+		
+		
+	}
+
+
+	public void printQueue() {
+		
+		MyLinkedList.printNodes();
+		
+	}
+
+
+	public INode peak() {
+		
+		return MyLinkedList.head;
+	}
+	
+	
+
+
+	public INode dequeue() {
+		
+	        return MyLinkedList.pop();
+	    }
+	}
