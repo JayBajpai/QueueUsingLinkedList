ace5b3f6f805c5003fc4e60a49a674146f8c5458
Jay22031999 <Jay22031999>
2021-08-29 13:46:20 +0530

    dequeue operation

--- QueueProject/src/queue/MyQueue.java
+++ QueueProject/src/queue/MyQueue.java
@@ -2,42 +2,22 @@ package queue;
 
 import linkedlist.INode;
 import stack.MyLinkedList;
-
 public class MyQueue {
-	
 	private final MyLinkedList MyLinkedList;
 	private INode myNode;
 	public MyQueue(){
-		
 		this.MyLinkedList = new MyLinkedList();
 	}
-	
-	
 	public void enqueue(INode myNode) {
-		
 		MyLinkedList.append(myNode);
-		
-		
 	}
-
-
 	public void printQueue() {
-		
-		MyLinkedList.printNodes();
-		
+		MyLinkedList.printNodes();	
 	}
-
-
 	public INode peak() {
-		
 		return MyLinkedList.head;
 	}
-	
-	
-
-
 	public INode dequeue() {
-		
 	        return MyLinkedList.pop();
-	    }
-	}
+    }
+}
